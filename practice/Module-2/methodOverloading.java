57272a1a64c3086213d344a67c18f167ca5e1fd8
Brandon Robare <149986922+BrandonRobare@users.noreply.github.com>
2026-09-01 16:20:24 -0400

    added method overloading example

---
+++ practice/Module-2/methodOverloading.java
@@ -0,0 +1,17 @@
+class methodCalculator {
+    // Method 1: two int parameters
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method 2: three int parameters
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Method 3: two double parameters
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
