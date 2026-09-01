b859a9879d8f09efecaa3094e3f0b0042fd1392b
Brandon Robare <149986922+BrandonRobare@users.noreply.github.com>
2026-09-01 15:17:55 -0400

    finished M2E3

--- examples/module-02-exercises/LoopsDemo.java
+++ examples/module-02-exercises/LoopsDemo.java
@@ -3,9 +3,30 @@ import java.util.Scanner;
 public class LoopsDemo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // TODO: for loop demo (known count from input)
-        // TODO: while loop demo
-        // TODO: do-while demo (runs at least once)
-        throw new UnsupportedOperationException("TODO");
+
+        // DONE: for loop
+        System.out.println("Multiplication table for 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("5 x " + i + " = " + (5 * i));
+        }
+
+        // DONE: while loop
+        int count = 5;
+        while (count >= 0) {
+            System.out.println("countdown: " + count);
+            --count;
+        }
+
+        // DONE: do-while
+        String choice;
+        do {
+            System.out.print("Type 'menu' to see it again, anything else to quit: ");
+            choice = scanner.nextLine();
+            if (choice.equals("menu")) {
+                System.out.println("1) Add  2) Withdraw  3) Exit");
+            }
+        } while (choice.equals("menu"));
+
+        scanner.close();
     }
 }
