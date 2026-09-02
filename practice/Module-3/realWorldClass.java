5ffd5257d1a2429fe771e64b75d2b2b1d22085a5
Brandon Robare <149986922+BrandonRobare@users.noreply.github.com>
2026-09-02 10:47:16 -0400

    added real world example of object oriented programming

---
+++ practice/Module-3/realWorldClass.java
@@ -0,0 +1,23 @@
+class Person {
+    private String name;
+    private int age;
+
+    public Person (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void introduce () {
+        System.out.println("Hi, I am " + name + " and I am " + age + " years old .");
+    }
+
+    public int getAge() { return age; }
+}
+
+public class realWorldClass {
+    public static void main(String[] args) {
+        Person p1 = new Person("Alice", 21);
+        p1.introduce();
+        System.out.println("Age: " + p1.getAge());
+    }
+}
