52951d1f6197b9f322b3aea998fa7de8e5592522
Brandon Robare <149986922+BrandonRobare@users.noreply.github.com>
2026-09-02 11:20:56 -0400

    added car class definition and creating object example

---
+++ practice/Module-3/classCar.java
@@ -0,0 +1,37 @@
+// slide 11
+// class definitions
+class Car {
+    int speed;
+    String color;
+    String model;
+
+    // methods (behavior)
+    void start() {
+        System.out.println("Car is starting...");
+    }
+
+    void stop() {
+        System.out.println("Car is stopping...");
+    }
+}
+
+// slide 12
+//creating objects
+public class classCar {
+    public static void main(String[] args) {
+
+        //creating objects of class Car
+        Car myCar = new Car();
+        Car yourCar = new Car();
+        Car officeCar = new Car();
+
+        // setting state
+        myCar.speed = 120;
+        myCar.color = "Red";
+        myCar.model = "Toyota Camry";
+
+        // Calling methods
+        myCar.start();
+        myCar.stop();
+    }
+}
