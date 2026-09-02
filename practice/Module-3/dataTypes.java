bdb0e7417ec702bafbb60c47ca5c5860b80d0216
Brandon Robare <149986922+BrandonRobare@users.noreply.github.com>
2026-09-02 10:35:14 -0400

    added M3 practice folder and first practice problem

---
+++ practice/Module-3/dataTypes.java
@@ -0,0 +1,21 @@
+// slide 3
+
+class Car {
+    String color;
+    int speed;
+
+    void drive() {
+        System.out.println("Driving");
+    }
+}
+
+public class dataTypes {
+    public static void main(String[] args) {
+        Car myCar = new Car(); //obj
+        myCar.color = "Red";
+        myCar.speed = 120;
+
+        System.out.println(myCar.color + " car doing " + myCar.speed);
+        myCar.drive();
+    }
+}
