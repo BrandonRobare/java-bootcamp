eff1c539ddc3277eee90f737873195e6a1894d71
Brandon Robare <149986922+BrandonRobare@users.noreply.github.com>
2026-09-03 10:20:42 -0400

    finished M4E3

--- examples/module-04-exercises/GcObserve.java
+++ examples/module-04-exercises/GcObserve.java
@@ -4,20 +4,25 @@ public class GcObserve {
 
         for (int round = 1; round <= 20; round++) {
             // About 12.5 MB per temporary batch.
-            // TODO: byte[][] batch = new byte[200][];
+            // DONE: byte[][] batch = new byte[200][];
+            byte[][] batch = new byte[200][];
 
             for (int i = 0; i < batch.length; i++) {
-                // TODO: batch[i] = new byte[64 * 1024];
-                // TODO: checksum += batch[i].length;
+                // DONE: batch[i] = new byte[64 * 1024];
+                batch[i] = new byte[64 * 1024];
+                // DONE: checksum += batch[i].length;
+                checksum += batch[i].length;
             }
 
             if (round % 5 == 0) {
-                // TODO: println "Completed round " + round
+                // DONE: println "Completed round " + round
+                System.out.println("Completed round " + round);
             }
 
             // On the next iteration, this batch can become unreachable.
         }
 
-        // TODO: println "Allocated bytes over time: " + checksum
+        // DONE: println "Allocated bytes over time: " + checksum
+        System.out.println("Allocated bytes over time: " + checksum);
     }
 }
