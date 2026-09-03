ccfac1cf586f63cbd1795696cb6e9d8bd2ad7747
Brandon Robare <149986922+BrandonRobare@users.noreply.github.com>
2026-09-03 15:44:45 -0400

    added hashSet example

---
+++ practice/Module-5/hashSet.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class hashSet {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("Apple");
+        set.add("Banana");
+        set.add("Cherry");
+        set.add("Apple"); //ignored duplicate
+
+        set.add(null); // only one null kept
+        set.add(null);
+
+        System.out.println(set.contains("Banana"));
+        System.out.println(set.remove("Apple"));
+        System.out.println(set); // [Banana, null, Cherry]
+
+    }
+}
