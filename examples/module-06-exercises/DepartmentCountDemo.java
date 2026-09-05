cd56172b661e46abec437f99808fe8b9b3380cd9
Brandon Robare <149986922+BrandonRobare@users.noreply.github.com>
2026-09-05 12:39:52 -0400

    finished M6E6

--- examples/module-06-exercises/DepartmentCountDemo.java
+++ examples/module-06-exercises/DepartmentCountDemo.java
@@ -1,17 +1,22 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class DepartmentCountDemo {
     public static void main(String[] args) {
         List<Employee> employees = EmployeeData.sample();
 
-        // TODO: groupingBy department + counting
+        // DONE: groupingBy department + counting
         Map<String, Long> counts = employees.stream()
-                // TODO: .collect(Collectors.groupingBy(...))
-                ;
+                // DONE: .collect(Collectors.groupingBy(...))
+                .collect(Collectors.groupingBy(Employee::department, Collectors.counting()));
 
-        counts.forEach((dept, count) ->
+        Map<String, Long> sortedCounts = new TreeMap<>(counts);
+
+        sortedCounts.forEach((dept, count) ->
                 System.out.println(dept + ": " + count));
     }
+
 }
