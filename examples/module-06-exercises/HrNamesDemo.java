4264ec4392ca8907ca01a1c8911b710d569c35bb
Brandon Robare <149986922+BrandonRobare@users.noreply.github.com>
2026-09-05 12:59:00 -0400

    M6E7 done

--- examples/module-06-exercises/HrNamesDemo.java
+++ examples/module-06-exercises/HrNamesDemo.java
@@ -4,13 +4,16 @@ public class HrNamesDemo {
     public static void main(String[] args) {
         List<Employee> employees = EmployeeData.sample();
 
-        // TODO: filter HR → map name → sorted → toList
+        // DONE: filter HR → map name → sorted → toList
         List<String> hrNames = employees.stream()
-                // TODO: .filter(...)
-                // TODO: .map(...)
-                // TODO: .sorted()
-                // TODO: .toList()
-                ;
+                // DONE: .filter(...)
+                .filter(e -> e.department().equals("HR"))
+                // DONE: .map(...)
+                .map(Employee::name) // also can use .map(e -> e.name) instead of method reference
+                // DONE: .sorted()
+                .sorted()
+                // DONE: .toList()
+                .toList();
 
         System.out.println("HR names:");
         hrNames.forEach(System.out::println);
